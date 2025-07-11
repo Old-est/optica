ef208ccc58fa30532d8f51d67f2e486c714a1cf3
Old-est <d.tupitsyn.side@gmail.com>
2025-07-11 10:48:35 +0400

    added doc

--- include/optica/impl/type_parsers.hpp
+++ include/optica/impl/type_parsers.hpp
@@ -1,10 +1,16 @@
 #pragma once
 
 #include <charconv>
-#include <optional>
 #include <string_view>
 namespace optica {
 
+/**
+ * @brief Parsing function for std::string_view
+ *
+ * @tparam T Type into string should be parsed
+ * @param data Actual std::string_view with data
+ * @return bool that shows is parsing successful
+ */
 template <typename T> constexpr bool ParseType(std::string_view data, T &) {
   static_assert(sizeof(T) == 0, "No try_parse specialization for this type T");
   return false;
--- include/optica/impl/utils.hpp
+++ include/optica/impl/utils.hpp
@@ -3,6 +3,7 @@
 #include <ranges>
 #include <string_view>
 #include <vector>
+
 namespace optica::utils {
 
 inline std::vector<std::string_view> SplitString(std::string_view data,
