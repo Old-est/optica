0f8439a218f9d6db1de1e8f3a55c237a784c7f40
Old-est <d.tupitsyn.side@gmail.com>
2025-07-21 13:36:01 +0400

    test trigger

--- tests/single_option.cpp
+++ tests/single_option.cpp
@@ -15,7 +15,7 @@ TEST_CASE("Option can be parsed by long name with =", "[option]") {
   REQUIRE(result.Get<"day">().value() == 7);
 }
 TEST_CASE("Option can be parsed by short name", "[option]") {
-  constexpr std::string_view cmd = "-d 13";
+  constexpr std::string_view cmd = "-d 42";
   auto result = parser.Parse(cmd);
-  REQUIRE(result.Get<"day">().value() == 13);
+  REQUIRE(result.Get<"day">().value() == 10);
 }
